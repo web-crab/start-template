a421bc52de55ccf06303fa77c66e720fe9fc5999
Unknown <andrey.klimash@gmail.com>
2017-10-02 11:37:11 +0300

    remove scrollTo()

--- app/js/utils.js
+++ app/js/utils.js
@@ -1,13 +1,4 @@
-//  Функции
-
-function scrollTo(idElem, offset) {
-    const
-        elem = document.getElementById(idElem),
-        offsetTop = window.pageYOffset + elem.getBoundingClientRect().top;
-
-    document.body.scrollTop = offsetTop - offset;
-}
-
+//  Вспомогательные функции
 function serialize(obj) {
     let result = '';
     Object.keys(obj).forEach(key => result += key + '=' + obj[key] + '&');
